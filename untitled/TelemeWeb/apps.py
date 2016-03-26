1954dcaddaeca40e049e8f96235578ccc058cf5b
Diego Tobarra <dtbarra@gmail.com>
2016-03-26 21:20:30 +0100

    Subo la applicación tal y como la vimos por primera vez en cloud9 para sincronizarla con  GitHub

---
+++ untitled/TelemeWeb/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TelemewebConfig(AppConfig):
+    name = 'TelemeWeb'
---
+++ untitled/charts/views.py
@@ -0,0 +1,29 @@
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+
+import datetime
+
+
+def index(request):
+
+    # Construct a dictionary to pass to the template engine as its context
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context_dict)
+
+    ###################### UNREACHABLE #############################
+
+    # now = datetime.datetime.now()
+
+    ''' This is a multiline comment
+    html = "<html>" \
+       "<body><p>It is now %s.</p></body>" \
+       "</html>" % now
+
+    return HttpResponse(html)
+       '''
